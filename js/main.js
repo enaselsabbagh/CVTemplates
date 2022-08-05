b31e718003ca772583e4503760868e73407dc7aa
enas-98 <enaselsabbagh@gmail.com>
2022-08-05 23:28:35 +0300

    my changes

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+function myFunction1() {
+  alert("please wait page is loaded now " );
+}
+function myFunction() {
+  document.getElementById("demo").innerHTML = "You selected some text";
+}
+function color(color) {
+  document.forms[0].myInput.style.background = color;
+}
+function searchOf()
+{
+  window.location.href="search.html";
+
+}
